a033b925621fed2b45f42ca4fef78200d8eab367
mblls <mballas@villanova.edu>
2020-07-21 14:07:59 -0400

    working db and submit

---
+++ url_shortner/url_shortener/routes.py
@@ -0,0 +1,38 @@
+from flask import Blueprint, render_template, request
+from .models import Link
+from .extensions import db
+
+short = Blueprint('short', __name__)
+
+
+@short.route('/<short_url>')
+def redirect_to_url(short_url):
+    pass
+
+
+@short.route('/')
+def index():
+    return render_template('index.html')
+
+
+@short.route('/add_link', methods=['POST'])
+def add_link():
+    original_url = request.form['original_url']
+    link = Link(original_url=original_url)
+    # adding the original and shortened url to the db
+    db.session.add(link)
+    db.session.commit()
+
+    return render_template('link_added.html',
+                           new_link=link.short_url,
+                           original_url=link.original_url)
+
+
+@short.route('/stats')
+def stats():
+    pass
+
+
+@short.errorhandler(404)
+def page_not_found(e):
+    return '', 404
